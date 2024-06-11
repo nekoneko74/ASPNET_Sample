2aaf3e29a98fc86eadb34e433743696ac99fcc4e
nekoneko74 <suomi@integralworks.co.jp>
2024-06-11 10:56:37 +0900

    メッセージリスト中のプレースホルダを「%s」⇒「{0}」に変更しました。

--- ASPNET_Sample/common/Message.cs
+++ ASPNET_Sample/common/Message.cs
@@ -6,14 +6,14 @@
     public class Messages
     {
         // 成功メッセージ
-        public static string M0001 = "%sの新規登録が完了しました";
-        public static string M0002 = "%sの更新が完了しました";
-        public static string M0003 = "%sの削除が完了しました";
+        public static string M0001 = "{0}の新規登録が完了しました";
+        public static string M0002 = "{0}の更新が完了しました";
+        public static string M0003 = "{0}の削除が完了しました";
 
         // エラーメッセージ
-        public static string E0001 = "%sの新規登録に失敗しました";
-        public static string E0002 = "%sの更新に失敗しました";
-        public static string E0003 = "%sの削除に失敗しました";
+        public static string E0001 = "{0}の新規登録に失敗しました";
+        public static string E0002 = "{0}の更新に失敗しました";
+        public static string E0003 = "{0}の削除に失敗しました";
 
         public static string E9001 = "ログイン処理に失敗しました";
         public static string E9002 = "ログインアカウントとパスワードを入力してください";
